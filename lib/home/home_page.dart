4702016d6b18ee31f01b1b58304e6eff331f3d49
vrajraj gohil <vrajrajgohil11@gmail.com>
2019-10-15 13:46:17 +0530

    Initial commit

---
+++ lib/home/home_page.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import './bride.dart';
+import './haircare.dart';
+import './skin care.dart';
+import './spa.dart';
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:Colors.white70,
+                   //Appbar
+      appBar: AppBar(
+        elevation:20 ,
+        shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(15.0),side: BorderSide(color: Colors.yellow[300],width:2.0) ),
+        iconTheme: IconThemeData(color: Colors.black,size: 80),
+       backgroundColor: Colors.white,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('C',
+                style: TextStyle(color: Colors.black,fontSize: 40.0,fontWeight: FontWeight.w400,fontFamily: 'Cinzel'
+              ),),
+              Text('hameleon',
+                style: TextStyle(color: Colors.black,fontSize: 26.0,fontWeight: FontWeight.w400,letterSpacing: 2,
+              ),),
+              Text('S',
+                style: TextStyle(color: Colors.black,fontSize: 40.0,fontWeight: FontWeight.w400,fontFamily: 'Cinzel'
+              ),),
+              Text('tyling',
+                style: TextStyle(color: Colors.black,fontSize: 26.0,fontWeight: FontWeight.w400,letterSpacing:2),),
+            ],
+          ),
+         actions: <Widget>[
+           IconButton(icon: Icon(Icons.search), color: Colors.black,iconSize: 40,onPressed: ()=>{})
+         ],
+
+        ),
+        drawer:
+        new Drawer(
+          child:
+          new ListView(
+            children: <Widget>[
+              new ListTile(
+                title: Text("Appointment", style: new TextStyle(fontSize: 16,color: Colors.white ),),
+                trailing: new Icon(Icons.local_hospital),
+                onTap: ()=> Navigator.pushReplacement(context,new  MaterialPageRoute(builder: (context) => bride()),),
+              ),
+              Divider(
+                color: Colors.white,
+                height:50.0,
+              ),
+              new ListTile(
+                title: Text("Blogs", style: new TextStyle(fontSize: 16,color: Colors.white ),),
+                trailing: new Icon(Icons.assignment),
+                onTap: ()=> Navigator.pushReplacement(context,new  MaterialPageRoute(builder: (context) => new bride())),
+              ),
+              Divider(color: Colors.white,
+                height:50.0,),
+            ],
+          ),
+        ),
+                                      //GridView
+      body: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(10),
+        crossAxisSpacing: 5,
+        mainAxisSpacing: 5,
+        crossAxisCount: 2,
+        childAspectRatio: 0.70,
+
+        children: <Widget>[
+
+        /*  Container(
+            child: InkWell(onTap: ()=>{},child: Image(image: AssetImage('assets/menuPics/facial.jpeg'),fit: BoxFit.cover,))
+          ),
+          Container(
+              child: InkWell(onTap: ()=>{},child: Image(image: AssetImage('assets/menuPics/haircut.jpg'),fit: BoxFit.cover,))
+          ),*/
+          Card(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10),side: BorderSide(color: Colors.yellow[300],width:5)),
+            clipBehavior: Clip.hardEdge,
+            child: Container(
+
+                decoration: BoxDecoration(image:DecorationImage(
+                  colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.darken),
+                  image: AssetImage('assets/menuPics/haircut.jpg'),fit: BoxFit.cover,
+                )),
+                child: InkWell(onTap: ()=>{Navigator.push(context,new MaterialPageRoute(builder: (context) => new haircare()))},
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text('HAIR CARE',style: TextStyle(color: Colors.white,fontSize: 40,fontWeight: FontWeight.w600,fontFamily:'Cinzel'),textAlign: TextAlign.center,),
+                        Padding(padding: EdgeInsets.all(20)),
+                        Text('EXPLORE',style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.w900,fontFamily:'Cinzel'),textAlign: TextAlign.center,),
+                      ],
+                    )
+                )
+            ),
+          ),
+          Card(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10),side: BorderSide(color: Colors.yellow[300],width:5)),
+            clipBehavior: Clip.hardEdge,
+            child: Container(
+                decoration: BoxDecoration(image:DecorationImage(
+                  colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.darken),
+                  image: AssetImage('assets/menuPics/facial.jpeg'),fit: BoxFit.cover,
+                )),
+                child: InkWell(onTap: ()=>{Navigator.push(context,new MaterialPageRoute(builder: (context) => new skincare()))},
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text('SKIN CARE',style: TextStyle(color: Colors.white,fontSize: 40,fontWeight: FontWeight.w700,fontFamily:'Cinzel'),textAlign: TextAlign.center,),
+                        Padding(padding: EdgeInsets.all(20)),
+                        Text('EXPLORE',style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.w700,fontFamily:'Cinzel'),textAlign: TextAlign.center,),
+                      ],
+                    )
+                )
+            ),
+          ),
+          Card(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10),side: BorderSide(color: Colors.yellow[300],width:5)),
+            clipBehavior: Clip.hardEdge,
+            child: Container(
+                decoration: BoxDecoration(image:DecorationImage(
+                  colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.darken),
+                  image: AssetImage('assets/menuPics/spa.jpg'),fit: BoxFit.cover,
+                )),
+                child: InkWell(onTap: ()=>{Navigator.push(context,new MaterialPageRoute(builder: (context) => new spa()))},
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text('HANDS & FEET',style: TextStyle(color: Colors.white,fontSize: 35,fontWeight: FontWeight.w500,fontFamily: 'Cinzel'),textAlign: TextAlign.center,),
+                        Text('AND SPA',style: TextStyle(color: Colors.white,fontSize: 35,fontWeight: FontWeight.w500,fontFamily: 'Cinzel'),textAlign: TextAlign.center,),
+                        Padding(padding: EdgeInsets.all(40)),
+                        Text('EXPLORE',style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.w400,fontFamily: 'Cinzel'),textAlign: TextAlign.center,),
+                      ],
+                    )
+                )
+            ),
+          ),
+
+          Card(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10),side: BorderSide(color: Colors.yellow[300],width:5)),
+            clipBehavior: Clip.hardEdge,
+            child: Container(
+                decoration: BoxDecoration(image:DecorationImage(
+                  colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.darken),
+                  image: AssetImage('assets/menuPics/bride.jpg'),fit: BoxFit.cover,
+                )),
+                child: InkWell(onTap: ()=>{Navigator.push(context,new MaterialPageRoute(builder: (context) => new bride()))},
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text('BRIDE-GROOM',style: TextStyle(color: Colors.white,fontSize: 32,fontWeight: FontWeight.w500,fontFamily: 'Cinzel'),textAlign: TextAlign.center,),
+                        Text('AND',style: TextStyle(color: Colors.white,fontSize: 32,fontWeight: FontWeight.w500,fontFamily: 'Cinzel'),textAlign: TextAlign.center,),
+                        Text('PARTY MAKEUP',style: TextStyle(color: Colors.white,fontSize: 32,fontWeight: FontWeight.w500,fontFamily: 'Cinzel'),textAlign: TextAlign.center,),
+                        Padding(padding: EdgeInsets.all(20)),
+                        Text('EXPLORE',style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.w400,fontFamily: 'Cinzel'),textAlign: TextAlign.center,),
+                      ],
+                    )
+                )
+            ),
+          ),
+          /*Container(
+              child: InkWell(onTap: ()=>{},child: Image(image: AssetImage('assets/menuPics/bride.jpg'),fit: BoxFit.cover,))
+          ),
+          Container(
+              child: InkWell(onTap: ()=>{},child: Image(image: AssetImage('assets/menuPics/spa.jpg'),fit: BoxFit.cover,))
+          ),*/
+
+        ],
+      ),
+
+
+      );
+
+  }
+}
---
+++ lib/home/skin care.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+class skincare extends StatefulWidget {
+  @override
+  _skincareState createState() => _skincareState();
+}
+
+class _skincareState extends State<skincare> {
+ 
+  
+  @override
+  Widget build(BuildContext context) {
+       return Scaffold(
+      appBar: AppBar(
+        actionsIconTheme: IconThemeData(color: Colors.yellow[300]),
+           elevation: 20,
+          bottomOpacity: 20,
+          shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(15.0),side: BorderSide(color: Colors.yellow[300],width:2.0) ),
+
+          backgroundColor: Colors.brown[500],
+        title: Text('Skin Care',style: TextStyle(color: Colors.white, fontSize: 35.0, fontWeight: FontWeight.w400,fontFamily: 'Cinzel'),),
+        iconTheme: IconThemeData(color: Colors.black,size: 40,opacity: 20)
+      ),
+      body:
+      StreamBuilder<QuerySnapshot>(
+              stream: Firestore.instance.collection('Skincare').snapshots(),
+              // ignore: missing_return
+              builder: (context,snapshot) {
+                print("Data : ${snapshot.data}");
+                
+                
+                if (!snapshot.hasData) {
+                  const Text('loading');
+                  return Container();
+                }
+                else{
+                return  ListView.builder(
+                    itemCount: snapshot.data.documents.length,
+                    itemBuilder: (context, index) {
+                      DocumentSnapshot mypost = snapshot.data.documents[index];
+                      return Card(
+                        color: Colors.brown[500],
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                            side: BorderSide(
+                                color: Colors.yellow[300], width: 5)),
+                        margin: EdgeInsets.only(
+                            bottom: 150, left: 10, right: 10, top: 10),
+                        elevation: 20,
+                        clipBehavior: Clip.hardEdge,
+                        child: Container(
+
+
+                            padding: EdgeInsets.only(left: 15, right: 10),
+                           // height: 600,
+                            width: 600,
+
+
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+
+                              // mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Padding(padding: EdgeInsets.all(20)),
+                                Text('${mypost['Title']}', style: TextStyle(
+                                    color: Colors.yellow[300],
+                                    fontSize: 50,
+                                    fontFamily: 'Cinzel'),),
+                                Padding(padding: EdgeInsets.only(top: 25,)),
+                                Divider(color: Colors.yellow[300],
+                                  thickness: 1.5,
+                                  height: 10,
+                                  indent: 25,
+                                  endIndent: 25,),
+                                Padding(padding: EdgeInsets.all(20)),
+                                // Text('Description: "',style: TextStyle(color: Colors.white,fontSize:30,fontFamily:'Lobster'),),
+                                Text('${mypost['Description']}',
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(color: Colors.white,
+                                      fontWeight: FontWeight.w100,
+                                      fontSize:25,
+                                      fontFamily: 'Lobster',
+                                      letterSpacing: 2),),
+                                Padding(padding: EdgeInsets.all(15)),
+                                Row(children: <Widget>[Padding(
+                                    padding: EdgeInsets.only(left: 5)),
+                                  Text('Varies | ', style: TextStyle(
+                                      color: Colors.yellow[300],
+                                      fontSize: 30,
+                                      fontFamily: 'Cinzel'),),
+                                  Text('${mypost['Varies']}', style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 30,
+                                      fontFamily: 'Cinzel'),),
+                                  Padding(padding: EdgeInsets.only(left: 120)),
+                                  Text('Price | ', style: TextStyle(
+                                      color: Colors.yellow[300],
+                                      fontSize: 30,
+                                      fontFamily: 'Cinzel'),),
+                                  Text('${mypost['Cost']}', style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 30,
+                                      fontFamily: 'Cinzel'),),
+                                ],),
+                                Padding(padding: EdgeInsets.all(10)),
+                              ],
+                            )),
+                      );
+                    }
+                );
+                }
+              }
+              ),
+             );
+           }
+}
